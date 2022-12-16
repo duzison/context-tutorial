34abcf5819ceb90cb6f385cdc676a8b770e6aeba
sojin.son <sojin.son96@gmail.com>
2022-12-16 21:58:21 +0900

    15.4 Consumer 대신 Hook 또는 static contextType 사용하기

--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,31 +1,39 @@
-import {ColorConsumer} from '../contexts/color';
+import {Component} from 'react';
+import ColorContext from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColors =() => {
-    return(
-        <div>
-            <h2>색상을 선택하세요. </h2>
-            <ColorConsumer>
-                {({actions}) => (
+class SelectColors extends Component {
+    static contextType = ColorContext;
+
+    handleSetColor = color => {
+        this.context.actions.setColor(color);
+    };
+
+    handleSetSubColor = subcolor => {
+        this.context.actions.setSubcolor(subcolor);
+    };
+
+    render() {
+        return (
+            <div>
+               <h2>색상을 선택하세요. </h2> 
                     <div style ={{display:'flex'}}>
                             {colors.map(color => (
                                 <div
                                     key = {color}
                                     style ={{background:color, width:'24px', height: '24px', cursor: 'pointer'}}
-                                    onClick={()=> actions.setColor(color)}
-                                    onContextMenu={e => {
+                                    onClick = {() => this.handleSetColor(color)}
+                                    onContextMenu = {e => {
                                         e.preventDefault();
-                                        actions.setSubcolor(color);
+                                        this.handleSetSubColor(color);
                                     }}
                                 />
                             ))}
                     </div>
-                )}
-            </ColorConsumer>
-            <hr />
-         </div>
-    );
-};
+            </div>
+        );
+    }
+}
 
 export default SelectColors;
